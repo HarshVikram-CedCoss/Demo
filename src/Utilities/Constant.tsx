0ac258f25c6692bba6e7be9f8be259d44848d489
Harsh <harshvikramsingh@Harsh-MacBook.local>
2023-12-27 00:32:52 +0530

    Image changes

--- src/Utilities/Constant.tsx
+++ src/Utilities/Constant.tsx
@@ -1,5 +1,5 @@
 export const LOGO_URL =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWI8r3fx9UXIgGIV46IAS8nVR9MmFy8Rh25Q&usqp=CAU";
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVsXywO5899p20sAWV-g53q0ojEduiUZ5PTQ&usqp=CAU";
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 export const API =
