f547f30855d0d8f9f0579f947b050ba463577858
Harsh <harshvikramsingh@Harsh-MacBook.local>
2023-12-27 00:28:14 +0530

    Context Created and showcase added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import { LOGO_URL } from "../Utilities/Constant";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import useOnlineStatus from "../Utilities/useOnlineStatus";
+import UserContext from "../Utilities/UserContext";
 const Header = () => {
   const [isUserlogin, setisUserLogin] = useState<string>("Login");
   const isOnline = useOnlineStatus();
+  const { loggedInUser } = useContext<any>(UserContext); //getting the default userName
+  console.log(loggedInUser, "name");
   return (
     <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
       <div className="logo-container">
-        <img className="w-48" src={LOGO_URL} alt="logo" />
+        <img className="w-40" src={LOGO_URL} alt="logo" />
       </div>
       <div className="flex items-center">
         <ul className="flex p-4 m-4">
@@ -29,8 +32,7 @@ const Header = () => {
           >
             {isUserlogin}
           </button>
-
-          {/* <li className="px-4 ">{loggedInUser}</li> */}
+          <li className="px-4 font-semibold font-serif">{loggedInUser}</li>
         </ul>
       </div>
     </div>
